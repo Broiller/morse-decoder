a11cc721638d398ec760c7b2be3c22d54bd4a5a3
Yauheni Haurylenka <djonkaupervud@mail.ru>
2022-02-10 23:10:23 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    let simbol;
+
+    for (let i = 0; i <  expr.length; i += 10) {
+        simbol = [(expr.substr(i, 10))];
+        arr.push(simbol);
+    }
+
+
+    let dashPoint = [];
+    arr.forEach((element, index) => {
+        dashPoint[index] = '';
+
+        for (let i = 0; i < 10; i += 2) {
+            simbol = (element[0].substr(i, 2));
+            if (simbol === '10') {
+                dashPoint[index] += '.';
+            }
+            else if (simbol === '11') {
+                dashPoint[index] += '-';
+            }
+        }
+    });
+
+
+    let risult = dashPoint.map(function(element) {
+        if (element !== '') {
+            return MORSE_TABLE[element];
+        }
+        else {
+            return ' ';
+        }
+    }).join('');
+    
+
+
+    return risult;
+
 }
 
 module.exports = {
